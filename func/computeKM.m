07772febda9b479bd2ab766896df168ebbf41dbd
iqiukp <44639227+iqiukp@users.noreply.github.com>
2019-04-18 23:28:10 +0800

    Update computeKM.m

--- func/computeKM.m
+++ func/computeKM.m
@@ -7,7 +7,10 @@ function [K] = computeKM(x,y,sigma)
 % n1,n2: number of iuput samples
 % d: characteristic dimension of the samples
 % K: kernelMatrix (n1¡Án2)
+%
+% Created on 18th April 2019, by Kepeng Qiu.
 %-------------------------------------------------------------%
+
 sx = sum(x.^2,2);
 sy = sum(y.^2,2);
 K = exp((bsxfun(@minus,bsxfun(@minus,2*x*y',sx),sy'))/sigma^2);
